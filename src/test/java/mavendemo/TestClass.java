24fb500844b01f08357bd7afc88617fb895f8edf
Kusum <kusum111@in.ibm.com>
2018-08-06 15:49:14 +0000

    added WebDriverManager Dependency and its usage

--- src/test/java/mavendemo/TestClass.java
+++ src/test/java/mavendemo/TestClass.java
@@ -1,7 +1,7 @@
 package mavendemo;
 
 import static org.junit.Assert.*;
-
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,10 +11,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestClass {
 
   WebDriver driver=null;
-  
+ 
+ @BeforeClass
+  public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+  }
+    
   @Before
   public void setUp() {
 	 // System.setProperty("webdriver.chrome.driver", "C:\\JAVASE8Workspace\\chromedriver_win32 (1)\\chromedriver.exe");
+//	  ChromeDriverManager.getInstance().setup();
 	  driver = new ChromeDriver();
   }
 
