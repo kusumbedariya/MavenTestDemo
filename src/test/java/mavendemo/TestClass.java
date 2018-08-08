a30d70ad4d41e8f2d9cfc0b1bba58088223c5e8a
Kusum <kusum111@in.ibm.com>
2018-08-08 11:13:48 +0000

    missing import java.net.URL;

--- src/test/java/mavendemo/TestClass.java
+++ src/test/java/mavendemo/TestClass.java
@@ -5,6 +5,7 @@ import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.*;
+import java.net.URL;
 
 public class TestClass {
 
