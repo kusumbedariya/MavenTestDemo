852fd210505078ac5e2850846ccdf9bc91479544
Kusum <kusum111@in.ibm.com>
2018-08-08 10:00:47 +0000

    adding sauce labs integration

--- src/test/java/mavendemo/TestClass.java
+++ src/test/java/mavendemo/TestClass.java
@@ -5,24 +5,37 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestClass {
 
-  WebDriver driver;
+
+  public static final String USERNAME = "kusumbedariya";
+  public static final String ACCESS_KEY = "bb1f0c41-cae7-435d-849f-7b9658979977";
+  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
  
+   WebDriver driver;
+   
  @BeforeClass
   public static void setupClass() 
   {
 		System.out.println("@BeforeClass - SetupClass()");
-	 	  WebDriverManager.chromedriver().setup();
+//	 	WebDriverManager.chromedriver().setup();
   }
     
   @Before
   public void setUp() {
 	System.out.println("@Before - setUp()");
 	 // System.setProperty("webdriver.chrome.driver", "C:\\JAVASE8Workspace\\chromedriver_win32 (1)\\chromedriver.exe");
-// ChromeDriverManager.getInstance().setup();
-	 driver = new ChromeDriver();
+	// ChromeDriverManager.getInstance().setup();
+	    DesiredCapabilities caps = DesiredCapabilities.chrome();
+	    caps.setCapability("platform", "Windows 10");
+	    caps.setCapability("version", "latest");
+	 
+	    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+
+	//driver = new ChromeDriver();
   }
 
   @After
