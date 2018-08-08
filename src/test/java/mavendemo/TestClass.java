c74e43ab96f50efd426e9407b43250ff11b73343
Kusum <kusum111@in.ibm.com>
2018-08-08 10:18:14 +0000

    commit

--- src/test/java/mavendemo/TestClass.java
+++ src/test/java/mavendemo/TestClass.java
@@ -14,38 +14,38 @@ public class TestClass {
   public static final String ACCESS_KEY = "bb1f0c41-cae7-435d-849f-7b9658979977";
   public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
  
-   WebDriver driver;
+ private WebDriver driver;
    
  @BeforeClass
-  public static void setupClass() 
+  public static void setupClass() throws Exception 
   {
 		System.out.println("@BeforeClass - SetupClass()");
 //	 	WebDriverManager.chromedriver().setup();
   }
     
   @Before
-  public void setUp() {
+  public void setUp() throws Exception {
 	System.out.println("@Before - setUp()");
 	 // System.setProperty("webdriver.chrome.driver", "C:\\JAVASE8Workspace\\chromedriver_win32 (1)\\chromedriver.exe");
 	// ChromeDriverManager.getInstance().setup();
 	    DesiredCapabilities caps = DesiredCapabilities.chrome();
-	 caps.setCapability("platform", "Windows 10");
+	 	caps.setCapability("platform", "Windows 7");
 	    caps.setCapability("version", "latest");
 	 
 	    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
-	 System.out.println("driver is "+driver);
+	 	System.out.println("driver is "+driver);
 	//driver = new ChromeDriver();
   }
 
   @After
-  public void tearDown() {
+  public void tearDown() throws Exception {
   	System.out.println("@After - tearDown()");
   	//System.out.println("Driver - tear down : "+driver);
-    //driver.quit();
+    driver.quit();
   }
 
   @Test
-  public void testScript() {
+  public void testScript() throws Exception {
  System.out.println("@Test - testScript");
 //    HomePage homePage = new HomePage(driver);
 // 	homePage.open();
