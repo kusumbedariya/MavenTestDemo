f9a97f41c59d0bee24ba0d5513830ea40bd38dc9
Kusum <kusum111@in.ibm.com>
2018-08-10 12:31:27 +0000

    commit

--- src/test/java/mavendemo/TestClass.java
+++ src/test/java/mavendemo/TestClass.java
@@ -33,8 +33,8 @@ public class TestClass {
 //	 ChromeOptions options = new ChromeOptions();
 //	 options.addArguments("--lang=en");
 //	 caps.setCapability(ChromeOptions.CAPABILITY, options);
-	  caps.setPlatform(Platform.WINDOWS);
-	  caps.setVersion("latest");
+	  caps.setCapability("platform", "WINDOWS");
+	  caps.setCapability("version", "latest");
 	 caps.setCapability("extendedDebugging", "true");
 //	 WebDriver driver = new ChromeDriver(options)
 
