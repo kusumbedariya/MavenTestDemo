9dc8361a883c620fe1a9c1608158831700ac371f
Kusum <kusum111@in.ibm.com>
2018-08-13 09:47:48 +0000

    retry

--- src/test/java/mavendemo/SampleSauceTest.java
+++ src/test/java/mavendemo/SampleSauceTest.java
@@ -1,30 +1,46 @@
+package mavendemo;
+
+import static org.junit.Assert.assertEquals;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
- 
-import java.net.URL;
- 
-public class SampleSauceTest {
- 
-  public static final String USERNAME = "kusumbedariya";
-  public static final String ACCESS_KEY = "bb1f0c41-cae7-435d-849f-7b9658979977";
-  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
- 
-  public static void main(String[] args) throws Exception {
- 
-    DesiredCapabilities caps = DesiredCapabilities.chrome();
-    caps.setCapability("platform", "LINUX");
-    caps.setCapability("version", "latest");
- 
-    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
- 
-    /**
-     * Goes to Sauce Lab's guinea-pig page and prints title
-     */
- 
-    driver.get("https://saucelabs.com/test/guinea-pig");
-    System.out.println("title of page is: " + driver.getTitle());
- 
-    driver.quit();
-  }
+
+public class TestClass1 {
+	WebDriver driver;
+
+	  public static final String USERNAME = "kusumbedariya";
+	  public static final String ACCESS_KEY = "bb1f0c41-cae7-435d-849f-7b9658979977";
+	  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+	  
+	  @Before
+		public void setUp() throws MalformedURLException{
+		System.out.println("-- Setup() BEGIN --");
+		DesiredCapabilities caps = DesiredCapabilities.firefox();
+		caps.setCapability("platform", "LINUX");
+		caps.setCapability("version", "latest");
+		driver = new RemoteWebDriver(new URL(URL), caps);
+		System.out.println("Driver is : "+driver);
+		System.out.println("-- Setup() END --");
+	  }
+	  
+	  @Test
+	  public void testScript() {
+		System.out.println("-- testScript() BEGIN --");
+		driver.get("https://saucelabs.com/test/guinea-pig");
+		System.out.println("title of page is: " + driver.getTitle());
+	    String test="Kusum";
+	    assertEquals("Kusum", test);
+	    driver.quit();
+		System.out.println("-- testScript() END --");
+	  }
+	 
+
 }
+
+
+
